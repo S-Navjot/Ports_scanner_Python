d1f8d8f14ce567544e8d77e68b1ca828d91e7542
S-Navjot <nav.singh@outlook.fr>
2024-08-06 09:51:26 +0200

    v1.1

--- main.py
+++ main.py
@@ -3,47 +3,85 @@ from datetime import datetime
 import os
 from concurrent.futures import ThreadPoolExecutor
 
+#Function to clear Terminal. Working on Windonws (cls) and MAC/Linux (clear)
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 def host_target():
-    user_input_ip = input("Enter an IP Address (IPv4) : ")
-    hostname = socket.gethostbyname(user_input_ip)
-    print(f"Target : {hostname}")
-    return hostname
+    user_input_ip = input("Enter an IP Address (IPv4) or Domain name: ")
+    try:
+        hostname = socket.gethostbyname(user_input_ip)
+        print(f"Target: {hostname}")
+        return hostname
+    except socket.gaierror as e:
+        print(f"Error: {e}. Please enter a valid IP address or domain name.")
+        return host_target()
 
-def ports_to_scan():
-    user_input_first_port = int(input("Enter first port of port range : "))
-    user_input_last_port = int(input("Enter last port of port range : "))
-    print(f"Ports range to scan : {user_input_first_port} - {user_input_last_port}")
-    return [user_input_first_port, user_input_last_port]
+def get_ports_to_scan():
+    while True:
+        user_input = input("Enter ports to scan (e.g., 22, 80, 1000-2000): ")
+        print()
+        ports = []
+        ranges = user_input.split(',')
+        try:
+            for part in ranges:
+                if '-' in part:
+                    start, end = map(int, part.split('-'))
+                    if start < 1 or end > 65535:
+                        raise ValueError("Ports must be between 0 and 65535")
+                    ports.extend(range(start, end + 1))
+                else:
+                    port = int(part)
+                    if port < 1 or port > 65535:
+                        raise ValueError("Ports must be between 0 and 65535")
+                    ports.append(port)
+            return ports
+        except ValueError as e:
+            print(f"Error: {e}. Please enter valid ports.")
 
-def scan_port(target, port): #Function to scan 1 port
+def scan_port(target, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(1)
     result = s.connect_ex((target, port))
     s.close()
     return port, result
 
-def scan_ports(target, first_port, last_port):#Allow to scan multiples ports in parallele
+def scan_ports(target, ports):
     with ThreadPoolExecutor(max_workers=100) as executor:
-        ports = range(first_port, last_port + 1)
         futures = [executor.submit(scan_port, target, port) for port in ports]
         for future in futures:
             port, result = future.result()
             if result == 0:
-                print(f'Port {port} is [open]')
+                print(f'{target} : {port}/TCP is [open]')
 
 def scanners():
     while True:
+        clear_screen()
+        target = host_target()
+        if not target:
+            continue
+        ports = get_ports_to_scan()
+        start = datetime.now()
         try:
-            os.system('cls' if os.name == 'nt' else 'clear')
-            target = host_target()
-            ports = ports_to_scan()
-            start = datetime.now()
-            scan_ports(target, ports[0], ports[1])
+            scan_ports(target, ports)
         except Exception as e:
             print(f"Error: {e}")
         end = datetime.now()
-        print(f"Scan duration : {end - start}")
+        print(f"\nScan duration: {end - start}")
         break
 
 if __name__ == '__main__':
-    scanners()
+    clear_screen()
+    print("""Welcome in this TCP Ports scanner!
+          \nGithub : https://github.com/S-Navjot/Ports_scanner_Python/tree/main
+          \nPlease select your choice:""")
+    while True:
+        user_choice = int(input("""\n1 - Scan ports (single, range, or multiple)
+                                   \n2 - Quit the program 
+                                   \nSelect a choice between 1 and 2: """))
+        if user_choice == 1:
+            scanners()
+        elif user_choice == 2:
+            quit()
+        else:
+            print("\nError, you must select a number between 1 and 2")
